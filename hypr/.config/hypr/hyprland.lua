95c116521e45bb3293326e48e6f5611005bab91f
asifahamed-ece <asifahamed670@gmail.com>
2026-08-07 20:04:57 +0530

    Update disp settings and Scaling

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -9,10 +9,10 @@ require("windowrules")
 
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 hl.monitor({
-    output   = "",
-    mode     = "",
-    position = "",
-    scale    = "",
+    output   = "DP-1",
+    mode     = "1920x1080@144",
+    position = "0x0",
+    scale    = "1.33",
 })
 
 -------------------
